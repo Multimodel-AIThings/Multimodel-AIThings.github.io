7a1183dd528056057faa1065c7c86fc370753904
NamET584 <namET05082004@gmail.com>
2025-07-14 18:15:41 +0000

    Deploying to gh-pages from @ Multimodel-AIThings/Multimodel-AIThings.github.io@238ee7d6d981749f57564b52f27cfe7a51565bad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog-aithings-rg",
-          title: "Blog @ AiThings RG",
+  },{id: "nav-blog",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
