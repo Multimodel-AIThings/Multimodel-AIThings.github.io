bb67ddbb050d779f6876e69a1e219c36418c3ee0
NamET584 <namET05082004@gmail.com>
2025-07-14 17:50:07 +0000

    Deploying to gh-pages from @ Multimodel-AIThings/Multimodel-AIThings.github.io@0b0dfad046e0701dbd8fa5c45fd6bab20afc74ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-aithings-are-seeking-new-members",
+          title: 'AiThings are Seeking new members',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
